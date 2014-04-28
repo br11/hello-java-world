ccc38fa38b3b4763734dcb08dad74d21858f2170
Marcio Silva <silva.luis.marcio@gmail.com>
2014-04-28 09:43:57 -0300

    inicio

---
+++ App.java
@@ -0,0 +1,16 @@
+/**
+ * Sistema de saudações.
+ */
+public class App {
+
+	public static void main(String[] args) {
+		String visitorName = args[0];
+
+		GreetingsService service  = new GreetingsService();
+
+		String output = service.welcome(visitorName);
+
+		System.out.println(output);
+	}
+
+}
